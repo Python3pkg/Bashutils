f9ced91ab57c5d9208158840899a0178332cde05
Python3pkg <raliclo@gmail.com>
2017-05-17 23:02:28 -0700

    Convert to python3

--- bashutils/test/test_color.py
+++ bashutils/test/test_color.py
@@ -4,29 +4,29 @@ from bashutils import colors
 
 
 def test():
-    print "COLORS LIST :"
+    print("COLORS LIST :")
     for color in colors.COLOR_CODE_TEXT:
-        print colors.color_text("My Text", color), color
+        print(colors.color_text("My Text", color), color)
 
-    print ""
-    print "------------------------------"
-    print "BACKGROUND COLORS LIST :"
+    print("")
+    print("------------------------------")
+    print("BACKGROUND COLORS LIST :")
     for bcolor in colors.COLOR_CODE_BG:
-        print colors.color_text("My Text", bcolor=bcolor), bcolor
+        print(colors.color_text("My Text", bcolor=bcolor), bcolor)
 
-    print ""
-    print "------------------------------"
-    print "EFFECTS :"
+    print("")
+    print("------------------------------")
+    print("EFFECTS :")
     for effect in colors.COLOR_EFFET:
-        print colors.color_text("My Text", effect=effect), effect
+        print(colors.color_text("My Text", effect=effect), effect)
 
-    print ""
-    print "------------------------------"
-    print "ALL :"
+    print("")
+    print("------------------------------")
+    print("ALL :")
     for color in colors.COLOR_CODE_TEXT:
         for bcolor in colors.COLOR_CODE_BG:
             for effect in colors.COLOR_EFFET:
-                print colors.color_text("My Text", color, bcolor, effect), color.ljust(10), bcolor.ljust(10), effect
+                print(colors.color_text("My Text", color, bcolor, effect), color.ljust(10), bcolor.ljust(10), effect)
 
 
 if __name__ == "__main__":
